83fae19fc44c5024a4242c981b35b6fc0224643b
timmy-badejo <tyomibadejo@my.bcit.ca>
2025-12-10 23:15:10 -0800

    Update babel.config.js
    Updated babel.config.js to explicitly use babel-preset-expo with the hermes-stable transform profile so Babel/Metro can parse Hermes/Flow syntax like the match expression in VirtualView.js. This should clear the remaining bundling parse error.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      [
+        'babel-preset-expo',
+        // Explicitly opt into Hermes transform profile so Flow `match` syntax parses.
+        { unstable_transformProfile: 'hermes-stable' },
+      ],
+    ],
     plugins: ['react-native-reanimated/plugin'],
   };
 };
